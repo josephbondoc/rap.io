f38671141d5e1eaeed9ada51f76b2a67e36f71b7
joseph bondoc <josephsantosbondoc@gmail.com>
2019-05-14 23:53:01 -0700

    sad

--- rapios.js
+++ rapios.js
@@ -4,39 +4,30 @@ var rapios = {
     old: {
         title: "old school",
         image: ["img/blah.jpg", "img/blah2.jpg"],
-        button1: "blah",
-        button2: "blah2",
+        button1: "the stuggle",
+        button2: "braggadocio",
+        button3: "party",
         songs: []
     },
     gold: {
         title: "golden age",
+        button1:"west coast",
+        button2: "instrumental",
+        button3: "east coast"
     },
     mod: {
         title: "modern age",
-
-
+        button1: "alternative",
+        button2: "conscious",
+        button3: "trap"
     }
+
 }
 
 // CATAGORY SELECTION
 
-var catagory = {
-    oldschool: {
-        button1: "the stuggle",
-        button2: "braggadocio"
-    },
-    goldenage: {
-        button1:"West Coast",
-        button2: "Instrumental",
-        button3: "East Coast"
-    },
-    modern: {
-        button1: "Alt Hip-Hop",
-        button2: "Conscious Hip-Hop",
-        button3: "Trap"
-    }
+
     
 
 
 
-}
