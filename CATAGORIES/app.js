4d6aa445d0f845c299d3889c7408bf4e4676d389
joseph bondoc <josephsantosbondoc@gmail.com>
2019-05-15 04:48:06 -0700

    s

--- CATAGORIES/app.js
+++ CATAGORIES/app.js
@@ -1,10 +1,10 @@
 //function to show the artists via css transition
 function but1slide(){
-    document.querySelector(".artist1").style.left = "30px";
+    document.querySelector(".artist1").style.left = "20px";
     document.querySelector(".artist1").style.top = "35px";
-    document.querySelector(".artist2").style.left = "30px";
+    document.querySelector(".artist2").style.left = "20px";
     document.querySelector(".artist2").style.top = "55px";
-    document.querySelector(".artist3").style.left = "30px";
+    document.querySelector(".artist3").style.left = "20px";
     document.querySelector(".artist3").style.top = "75px";
 
     document.querySelector(".artist4").style.left = "-200px";
@@ -16,11 +16,11 @@ function but1slide(){
 }
 
 function but2slide(){
-    document.querySelector(".artist4").style.left = "150px";
+    document.querySelector(".artist4").style.left = "130px";
     document.querySelector(".artist4").style.top = "35px";
-    document.querySelector(".artist5").style.left = "150px";
+    document.querySelector(".artist5").style.left = "130px";
     document.querySelector(".artist5").style.top = "55px";
-    document.querySelector(".artist6").style.left = "150px";
+    document.querySelector(".artist6").style.left = "130px";
     document.querySelector(".artist6").style.top = "75px";
 
     document.querySelector(".artist1").style.left = "-200px";
@@ -32,11 +32,11 @@ function but2slide(){
 }
 
 function but3slide(){
-    document.querySelector(".artist7").style.left = "270px";
+    document.querySelector(".artist7").style.left = "250px";
     document.querySelector(".artist7").style.top = "35px";
-    document.querySelector(".artist8").style.left = "270px";
+    document.querySelector(".artist8").style.left = "250px";
     document.querySelector(".artist8").style.top = "55px";
-    document.querySelector(".artist9").style.left = "270px";
+    document.querySelector(".artist9").style.left = "250px";
     document.querySelector(".artist9").style.top = "75px";
 
     document.querySelector(".artist1").style.left = "-200px";
