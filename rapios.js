3ec1d047157627d8af00d2a0cb19907c77aafd7c
joseph bondoc <josephsantosbondoc@gmail.com>
2019-05-15 01:57:07 -0700

    d

--- rapios.js
+++ rapios.js
@@ -7,26 +7,68 @@ var rapios = {
         button1: "the stuggle",
         button2: "braggadocio",
         button3: "party",
-        songs: []
+        songs: [],
+
+        struggle1: "Grandmaster-Flash",
+        struggle2: "Sugerhill Gang",
+        struggle3: "Run DMC",
+
+        bragg1: "Public Enemy",
+        bragg2: "LL Cool J",
+        bragg3: "Slick Rick",
+
+        party1: "Afrika B.",
+        party2: "Kurtis Blow",
+        party3: "DJ Kool Herc"
+
     },
     gold: {
         title: "golden age",
         button1:"west coast",
         button2: "instrumental",
-        button3: "east coast"
+        button3: "east coast",
+
+        struggle1: "Snoop Dogg",
+        struggle2: "Dr.Dre",
+        struggle3: "Tupac",
+
+        bragg1: "J-Dilla",
+        bragg2: "MF Doom",
+        bragg3: "Nujabes",
+
+        party1: "Biggie Smalls",
+        party2: "Wu-Tang Clan",
+        party3: "Nas"
     },
+
     mod: {
         title: "modern age",
         button1: "alternative",
         button2: "conscious",
-        button3: "trap"
+        button3: "trap",
+
+        struggle1: "Kanye West",
+        struggle2: "Travis Scott",
+        struggle3: "BROCKHAMPTON",
+
+        bragg1: "Kendrick Lamar",
+        bragg2: "Frank Ocean",
+        bragg3: "J.Cole",
+
+        party1: "Waka Flocka",
+        party2: "Future",
+        party3: "Migos"
     }
+
 
 }
 
 // CATAGORY SELECTION
 
+var genres = {
+    
 
+}
     
 
 
